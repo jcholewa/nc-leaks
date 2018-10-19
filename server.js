1e829910dbec8afe68c8aa5e79c8a6d299e4f381
brlom <brlom007@gmail.com>
2018-10-19 11:37:37 +0100

    completed num 1

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const http = require('http');
 const fs = require('fs');
 
+
+
 const server = http.createServer((request, response) => {
-  console.log('hello');
+  if (request.url === '/api') {
+    if (request.method === 'GET') {
+      response.setHeader('Content-Type', 'application/json');
+      response.statusCode = 200;
+      response.write(JSON.stringify('Hello'));
+      response.end();
+    }
+  }
 });
 
 server.listen(9090);
