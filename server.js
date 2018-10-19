2a631fbdfc8247d1e30dc153733bb2a82ed0b745
brlom <brlom007@gmail.com>
2018-10-19 11:54:10 +0100

    northcoders url sorted

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 
-
-
 const server = http.createServer((request, response) => {
   if (request.url === '/api') {
     if (request.method === 'GET') {
@@ -12,6 +10,22 @@ const server = http.createServer((request, response) => {
       response.end();
     }
   }
+  if (request.url === '/api/northcoders')
+    if (request.method === 'GET') {
+      getNorthcoders(response);
+    };
 });
+
+function getNorthcoders(response) {
+  fs.readFile('../northcoders.json', 'utf-8', (err, data) => {
+    if (err) console.log(err);
+    else {
+      response.setHeader('Content-Type', 'application/json');
+      response.statusCode = 200;
+      response.write(data);
+      response.end();
+    };
+  });
+};
 
 server.listen(9090);
